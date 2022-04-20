aef3cf906723514a3f465891b58601db68085bf5
Ovi Trif <octrif@gmail.com>
2022-04-20 17:33:55 +0200

    Add annotation for Change

---
+++ retrosheet/src/main/kotlin/com/github/theapache64/retrosheet/annotations/Change.kt
@@ -0,0 +1,9 @@
+package com.github.theapache64.retrosheet.annotations
+
+/**
+ * Created by ovitrif : Wed 20 Apr,2021 @ 17:30
+ */
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention
+annotation class Change
